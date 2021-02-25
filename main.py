957915dff876c0c8e7aa7bbbf13b8b32a85ad2b8
Damindarov <damin4@mail.ru>
2021-02-26 00:57:03 +0300

    First method
    First method

--- main.py
+++ main.py
@@ -7,19 +7,15 @@ import time
 import pyrealsense2 as rs
 import numpy as np
 import cv2
-
-import pyautogui as pag
 # from yolo import YOLO
 
 
 import copy
 from pip._vendor.msgpack.fallback import xrange
 
-
 def distance(x1, y1, x2, y2):
-    c = np.math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    c = np.math.sqrt((x2-x1)**2 + (y2-y1)**2)
     return c
-
 
 def centroid(max_contour):
     if max_contour is not None:
@@ -38,7 +34,7 @@ def draw_circles(frame, traverse_point):
             cv2.circle(frame, traverse_point[i], int(5 - (5 * i * 3) / 100), [0, 255, 255], -1)
 
 
-def calculateFingers(res, drawing, xc, yc):
+def calculateFingers(res, drawing,xc,yc):
     #  convexity defect
     hull = cv2.convexHull(res, returnPoints=False)
 
@@ -64,9 +60,9 @@ def calculateFingers(res, drawing, xc, yc):
                 c = np.math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
                 angle = np.math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
 
-                if np.math.pi >= angle >= np.math.pi * 0.6:  # angle less than 90 degree, treat as fingers
+                if np.math.pi >= angle >= np.math.pi*0.6:  # angle less than 90 degree, treat as fingers
                     cnt += 1
-                    dist = distance(far[0], far[1], xc, yc)
+                    dist = distance(far[0],far[1],xc,yc)
 
                     if dist >= max:
                         ind = far
@@ -78,8 +74,9 @@ def calculateFingers(res, drawing, xc, yc):
 def print_hi(name):
     # Configure depth and color streams
     pipeline = rs.pipeline()
-    colorizer = rs.colorizer()
-    tr1 = rs.threshold_filter(min_dist=0.15, max_dist=0.9)
+    pc = rs.pointcloud()
+    tr = rs.threshold_filter(min_dist=0.15, max_dist=1.5)
+
     config = rs.config()
     # Get device product line for setting a supporting resolution
     pipeline_wrapper = rs.pipeline_wrapper(pipeline)
@@ -93,41 +90,76 @@ def print_hi(name):
         config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
     else:
         config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
     # Start streaming
     pipeline.start(config)
+    # threshold_filter = rs.threshold_filter()
+    # threshold_filter.set_option(rs.option.max_distance, 1)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
     size_p = 4
     point_list = np.zeros(size_p)
-    size_screen = pag.size()
-    print(size_screen[0])
-    coeff = [(size_screen[0]/440), size_screen[1]/380]
-    toutch = 0
+
     try:
         while True:
+
             # Wait for a coherent pair of frames: depth and color
             frames = pipeline.wait_for_frames()
+            # frames = tr.process(frames)
             depth_frame = frames.get_depth_frame()
             color_frame = frames.get_color_frame()
+            # distances = depth_frame.get_distance()
+            width = depth_frame.get_width()
+            height = depth_frame.get_height()
 
             if not depth_frame or not color_frame:
                 continue
 
-            colorized_depth = np.asanyarray(colorizer.colorize(tr1.process(depth_frame)).get_data())
-            colori_image = np.asanyarray(color_frame)
+            # Convert images to numpy arrays
+            depth_frame_copy = tr.process(depth_frame)
+            color_image_copy = tr.process(color_frame)
+
+            depth_image = np.asanyarray(depth_frame_copy.get_data())
+            color_image = np.asanyarray(color_image_copy.get_data())
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.05), cv2.COLORMAP_JET)
+
+            color_image = cv2.bilateralFilter(color_image, 10, 50, 100)  # Smoothing
+
+
+            # for i in range(vts.shape[0]):
+            #     print(vts[i])
+            # vts = vts[np.where(vts[:, 2] < 0.35)]
+
+
+
+            # color_image = cv2.bilateralFilter(color_image, 5, 50, 100)
+            # color_image = color_image[np.where(vts[:, 2] < 0.35)]
+
+            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+
+            cv2.imshow('depthColorMap',depth_colormap)
+            depth_colormap_dim = depth_colormap.shape
+            color_colormap_dim = color_image.shape
+
+
+            # color_image = depth_colormap
+
+
+
+            # dist_to_center = depth_frame.get_distance(int(width / 2), int(height / 2))
+            # print(dist_to_center)
+
+            # bgModel = cv2.createBackgroundSubtractorMOG2(0, 50)
+            # fgmask = bgModel.apply(color_image)
             # kernel = np.ones((3, 3), np.uint8)
-            # colorized_depth = cv2.erode(colorized_depth, kernel, iterations=1)
+            # fgmask = cv2.erode(fgmask, kernel, iterations=1)
+            # img = cv2.bitwise_and(color_image, color_image, mask=fgmask)
 
-            # cv2.imshow('Camera', colori_image)
-
-            color_image = cv2.bilateralFilter(colorized_depth, 10, 50, 100)  # Smoothing
-            # color_image = cv2.flip(color_image, 0)
-            color_image = cv2.flip(color_image, 1)#vertical
             bgModel = cv2.createBackgroundSubtractorMOG2(0, 50)
             fgmask = bgModel.apply(color_image)
             kernel = np.ones((3, 3), np.uint8)
             fgmask = cv2.erode(fgmask, kernel, iterations=1)
             img = cv2.bitwise_and(color_image, color_image, mask=fgmask)
-
-
 
             # Skin detect and thresholding
             hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -135,11 +167,19 @@ def print_hi(name):
             upper = np.array([20, 255, 255], dtype="uint8")
             skinMask = cv2.inRange(hsv, lower, upper)
             cv2.imshow('Threshold Hands', skinMask)
-            cv2.imshow('depth', color_image)
+
+
+            images_p = None
+            if depth_colormap_dim != color_colormap_dim:
+                resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]),
+                                                 interpolation=cv2.INTER_AREA)
+                images_p = np.hstack((resized_color_image, depth_colormap))
+            else:
+                images_p = np.hstack((color_image, depth_colormap))
+            cv2.imshow('depth', images_p)
+
             skinMask1 = copy.deepcopy(skinMask)
-
             contours, hierarchy = cv2.findContours(skinMask1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
             length = len(contours)
             maxArea = -1
             if length > 0:
@@ -150,37 +190,103 @@ def print_hi(name):
                         maxArea = area
                         ci = i
                         res = contours[ci]
+
                 hull = cv2.convexHull(res)
+
                 drawing = np.zeros(img.shape, np.uint8)
                 cv2.drawContours(drawing, [res], 0, (0, 255, 0), 2)
                 cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
-                cx, cy = centroid(res)  # center of mass
-
-                # pag.moveTo(cx*coeff[0], cy*coeff[0])
+                drawing1 = drawing.copy()
+                cx, cy = centroid(res)#center of mass
 
                 zDepth = depth_frame.get_distance(int(cx), int(cy))
-                point_list = np.append(point_list, zDepth)
-                point_list = point_list[1:(size_p + 1)]
+
+
+
+                point_list = np.append(point_list,zDepth)
+                point_list = point_list[1:(size_p+1)]
                 max_p = max(point_list)
                 min_p = min(point_list)
                 delta = max_p - min_p
-
-                # Добавить окружность для точек xy
-                if delta > 0.1 and min_p > 0 and list(point_list).index(min_p) < list(point_list).index(max_p):
+                #Добавить окружность для точек xy
+                if delta > 0.12 and min_p > 0 and list(point_list).index(min_p) < list(point_list).index(max_p):
                     point_list = np.zeros(size_p)
-                    toutch+=1
-                    # pag.click()
-                    print('toutch',toutch)
+                    print('toutch')
 
-                # calculateFingers(res, drawing, cx, cy)
+                calculateFingers(res, drawing1,cx,cy)
 
                 # chechcountr distacnes
 
-                if zDepth < 0.9:
-                    draw_circles(drawing, [(cx, cy)])
+                if zDepth < 3.9:
+                    draw_circles(drawing1, [(cx, cy)])
                 else:
-                    drawing = np.zeros(img.shape, np.uint8)
-                cv2.imshow('output', drawing)
+                    drawing1 = np.zeros(img.shape, np.uint8)
+                cv2.imshow('output', drawing1)
+
+            # #detectfaces and remove it
+            # gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+            # # Detect faces
+            # faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+            # # Draw rectangle around the faces
+            #
+            # for (x, y, w, h) in faces:
+            #     cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 0, 0), -1)
+            #     cv2.rectangle(skinMask, (x, y), (x + w, y + h), (0, 0, 0), -1)
+            #
+            # #detect skin
+            # hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
+            # lower = np.array([0, 48, 80], dtype="uint8")
+            # upper = np.array([20, 255, 255], dtype="uint8")
+            # skinMask = cv2.inRange(hsv, lower, upper)#colorFilter
+            #
+            #
+            #
+            # cv2.imshow('d',skinMask)
+            # cv2.imshow('d', color_image)
+            #
+
+            #
+            # bgModel = cv2.createBackgroundSubtractorMOG2(0, 50)
+            # fgmask = bgModel.apply(color_image)
+            # kernel = np.ones((3, 3), np.uint8)
+            # fgmask = cv2.erode(fgmask, kernel, iterations=1)
+            # img = cv2.bitwise_and(color_image, color_image, mask=fgmask)
+            #
+            # #detectfaces and remove it
+            # gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+            # # Detect faces
+            # faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+            # # Draw rectangle around the faces
+            #
+            # #detect skin
+            # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            # lower = np.array([0, 48, 80], dtype="uint8")
+            # upper = np.array([20, 255, 255], dtype="uint8")
+            # skinMask = cv2.inRange(hsv, lower, upper)
+            #
+            # for (x, y, w, h) in faces:
+            #     cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 0, 0), -1)
+            #     cv2.rectangle(skinMask, (x, y), (x + w, y + h), (0, 0, 0), -1)
+            #
+            # # Display the output
+            # cv2.imshow('img', color_image)
+            # cv2.imshow('Threshold Hands', skinMask)
+            #
+            # #depth_frame = frames.get_depth_frame()
+            # # zDepth = depth_frame.get_distance(int(1), int(1))
+            # # print(zDepth)
+            #
+            #
+            #
+            # # If depth and color resolutions are different, resize color image to match depth image for display
+            # if depth_colormap_dim != color_colormap_dim:
+            #     resized_color_image = cv2.resize(img, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]),
+            #                                      interpolation=cv2.INTER_AREA)
+            #     images = np.hstack((resized_color_image, depth_colormap))
+            # else:
+            #     images = np.hstack((color_image, depth_colormap))
+            #
+            # # Show images
 
             cv2.waitKey(1)
 
@@ -188,10 +294,6 @@ def print_hi(name):
 
         # Stop streaming
         pipeline.stop()
-
-    # threshold_filter = rs.threshold_filter()
-    # threshold_filter.set_option(rs.option.max_distance, 1)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
 # Press the green button in the gutter to run the script.
